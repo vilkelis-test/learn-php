b44a75b031e37b0c692f8a10e9c6c412a60f0b2a
vilkelis-test <it@cenyvaptekah.ru>
2018-06-18 16:36:24 +0500

    array lesson

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>
+<?php
+  echo 'Hello from Степан Голованов'
+?>
+</h1>
+ 
+ <ul>
+  <li><a href="/array.php">Массивы</a></li>
+ </ul> 	
+	
+</body>
+</html>
